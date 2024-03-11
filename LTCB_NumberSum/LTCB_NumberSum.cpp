3a136735ee9bf05d2f27eab8619bde8d641425d2
ADMIN <ADMIN@LAB1-MAY29>
2024-03-11 08:11:54 +0700

    Add project files.

---
+++ LTCB_NumberSum/LTCB_NumberSum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double tong = 0, so;
+	cout << "Nhap so: ";
+	while(so!=0){
+	cin >> so;
+	tong += so;
+}
+cout << "Tong= " << tong << endl;
+return 0;
+}
